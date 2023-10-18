c09c9c3187a0f63f40cd07ec8865cbb6cdec8226
MichaelChoudhury <MichaelChoudhury@users.noreply.github.com>
2023-10-18 00:05:35 +0000

    ep16 deployed with Akshay's code

--- src/utils/z_constants.js
+++ src/utils/z_constants.js
@@ -4,13 +4,13 @@ export const LOGO =
 export const USER_AVATAR =
   "https://occ-0-6247-2164.1.nflxso.net/dnm/api/v6/K6hjPJd6cR6FpVELC5Pd6ovHRSk/AAAABdpkabKqQAxyWzo6QW_ZnPz1IZLqlmNfK-t4L1VIeV1DY00JhLo_LMVFp936keDxj-V5UELAVJrU--iUUY2MaDxQSSO-0qw.png?r=e6e";
 
-export const API_OPTIONS = {
-  method: "GET",
-  headers: {
-    accept: "application/json",
-    Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJiYmZkNTkzNTNkN2ZlMzMxMTYwMGJiYjBmZWY3MGFmZSIsInN1YiI6IjY1MjBmOGUzMGNiMzM1MDExYjFiYzRmMiIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.8ryXy8jIYxah5mAIwGovBkZ5TVysFsTH18S4n-uf1Ic',
-  },
-};
+// export const API_OPTIONS = {
+//   method: "GET",
+//   headers: {
+//     accept: "application/json",
+//     Authorization: "Bearer " + process.env.REACT_APP_TMDB_KEY,
+//   },
+// };
 
 export const IMG_CDN_URL = "https://image.tmdb.org/t/p/w500";
 
--- src/utils/z_firebase.js
+++ src/utils/z_firebase.js
@@ -2,19 +2,20 @@
 import { initializeApp } from "firebase/app";
 import { getAnalytics } from "firebase/analytics";
 import { getAuth } from "firebase/auth";
+
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyCvv_2raJ3NDpehrU5h_ah6ohWVEoYV5KY",
-  authDomain: "mjc-007-f4dbb.firebaseapp.com",
-  projectId: "mjc-007-f4dbb",
-  storageBucket: "mjc-007-f4dbb.appspot.com",
-  messagingSenderId: "1020682336593",
-  appId: "1:1020682336593:web:8a9e23353bdf7c41e7fb70",
-  measurementId: "G-N1EE5SM1DG"
+  apiKey: "AIzaSyAwKbz6Wo1UVp1BzKSJVXV6hTz3vwy74k8",
+  authDomain: "netflixgpt-57a56.firebaseapp.com",
+  projectId: "netflixgpt-57a56",
+  storageBucket: "netflixgpt-57a56.appspot.com",
+  messagingSenderId: "1093133802935",
+  appId: "1:1093133802935:web:c284961959cce2a54adc32",
+  measurementId: "G-DEHFXGZ999",
 };
 
 // Initialize Firebase
